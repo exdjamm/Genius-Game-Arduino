7e6e9afd7d3a572a2f7217285ab450f16aa26b11
ExodoJaffar <exodojaffar@gmail.com>
2019-03-16 16:51:29 -0400

    Cores work in the global variable.

--- Genius.ino
+++ Genius.ino
@@ -1,29 +1,36 @@
-void ligaLeds(int vetor[]);
+void ligaLeds();
+
+int  cores[] = {2,3,2,2,3,2,3};
 
 #include "commands.h"
 
 // Pinos dos leds
+int ledY = 5;
 int ledG = 4;
-int ledY = 3;
-int ledB = 2; 
+int ledB = 3;
+int ledR = 2; 
 
 // Pinos dos botoes
+int buttonY = 11;
 int buttonG = 10;
-int buttonY = 9;
-int buttonB = 8;
+int buttonB = 9;
+int buttonR = 8;
 
 void setup() {
+  Serial.begin(9600);
 	// Declarando o modo dos pinos
 		// Leds
 		pinMode(ledG, OUTPUT);
 		pinMode(ledY, OUTPUT);
 		pinMode(ledB, OUTPUT);
+    pinMode(ledR, OUTPUT);
 		// Botoes
 		pinMode(buttonG, INPUT);
 		pinMode(buttonY, INPUT);
 		pinMode(buttonB, INPUT);
+    pinMode(buttonR, INPUT);
 }
 
 void loop() {
-  ligaLeds(cores);
+  ligaLeds();
 }

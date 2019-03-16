db82bec07e4e25f81a2d95392787499c4cd30ead
ExodoJaffar <exodojaffar@gmail.com>
2019-03-15 22:09:31 -0400

    function ligaled, now work in local var. function setup, now var i work in local.

--- Genius.ino
+++ Genius.ino
@@ -1,16 +1,16 @@
-void ligaLeds();
+void ligaLeds(int *vetor[]);
 
 #include "commands.h"
 
-//int botaos[4] = {2,3,4,5};// Botaos do Genius : 1=red;2=blue;3=green;4=yellow.
-int leds[4]   = {2,3,4}; // Leds do Genius : 1=red;2=blue;3=green;4=yellow.
-//int Nr;// Numero aleatorio para a cor.
-//int cores[10];// Vetor para salvar qual cor foi adicionada a memoria.
-//int Contar = 0;// Numero de onde esta o vetor.
+//int botaos[4] = {2,3,4,5}; Botaos do Genius : 1=red;2=blue;3=green;4=yellow.
+//int leds[4]   = {2,3,4}; Leds do Genius : 1=red;2=blue;3=green;4=yellow.
+//int Nr; Numero aleatorio para a cor.
+//int cores[10]; Vetor para salvar qual cor foi adicionada a memoria.
+//int Contar = 0; Numero de onde esta o vetor.
 
 void setup() {
 	// Declarando os pinos.
- Serial.begin(9600);
+	int i;
 	for (int i = 0; i <=3; ++i){
 		pinMode(leds[i], OUTPUT);
 	//	pinMode(botaos[i], INPUT);
@@ -18,5 +18,5 @@ void setup() {
 }
 
 void loop() {
-  ligaLeds();
+  ligaLeds(cores);
 }
--- commands.h
+++ commands.h
@@ -1,15 +1,11 @@
-int vetor[] = {2,3,4,2,3,4};
-int i = 0;
-
-void ligaLeds(){
-  i = 0;
-  while( i<= sizeof(vetor)/sizeof(int)){
-    digitalWrite(vetor[i], HIGH);
-    //Serial.println(sizeof(vetor)/sizeof(int));
-    delay(200);
-    digitalWrite(vetor[i], LOW);
-    delay(200);
-    i++;
+void ligaLeds(int *vetor[]){
+	int i = 0;
+	while( i<= sizeof(vetor)/sizeof(int)){
+	digitalWrite(vetor[i], HIGH);
+	//Serial.println(sizeof(vetor)/sizeof(int));
+	delay(200);
+	digitalWrite(vetor[i], LOW);
+	delay(200);
+	i++;
     }
-  i = 0;
-  }
+}

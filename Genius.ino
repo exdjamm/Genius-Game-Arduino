2a2914329b55552755adb6a489f9f7ee5cffce57
ExodoJaffar <exodojaffar@gmail.com>
2019-03-15 22:10:56 -0400

    Remove the comments

--- Genius.ino
+++ Genius.ino
@@ -2,12 +2,6 @@ void ligaLeds(int *vetor[]);
 
 #include "commands.h"
 
-//int botaos[4] = {2,3,4,5}; Botaos do Genius : 1=red;2=blue;3=green;4=yellow.
-//int leds[4]   = {2,3,4}; Leds do Genius : 1=red;2=blue;3=green;4=yellow.
-//int Nr; Numero aleatorio para a cor.
-//int cores[10]; Vetor para salvar qual cor foi adicionada a memoria.
-//int Contar = 0; Numero de onde esta o vetor.
-
 void setup() {
 	// Declarando os pinos.
 	int i;

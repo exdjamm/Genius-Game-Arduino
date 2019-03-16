69d39b8b89ac708ba9bc8f3124e96eccea68aeac
ExodoJaffar <exodojaffar@gmail.com>
2019-03-16 18:38:53 -0400

    Copy code

--- Genius.ino
+++ Genius.ino
@@ -1,8 +1,12 @@
-int buttons[2] = {8,9}; // Pinos dos botoes
-int lose_game = false;  // Se errou a sequencia
-int colors[100] = {};   // Sequencia
-int leds[2] = {2,3};    // Pinos dos leds
-int button_on = 0;      // Se o botao precionado foi o mesmo do led
+void checkSequence();
+void waitPlay();
+
+int buttons[4] = {8,9,10,11}; // Pinos dos botoes
+int leds[4] = {2,3,4,5};     // Pinos dos leds
+int lose_game = false;      // Se errou a sequencia
+int sequence_led = 0;      // O momento da sequencia do botao apertado
+int colors[100] = {};     // Sequencia
+int button_on = 0;       // Se o botao precionado foi o mesmo do led
 int level = 0;          // Rodada
 
 #include "commands.h"
@@ -11,17 +15,18 @@ void setup(){
   Serial.begin(9600);
   randomSeed(analogRead(0)); // Semente para criar o numeros aleatorios
 	// Declarando o modo dos pinos
-  for(int i = 0;i<2;++i){pinMode(leds[i], OUTPUT);} // Leds
-  for(int i = 0;i<2;++i){pinMode(buttons[i], INPUT);}  // Botoes
+  for(int i = 0;i<4;++i){pinMode(leds[i], OUTPUT);} // Leds
+  for(int i = 0;i<4;++i){pinMode(buttons[i], INPUT);}  // Botoes
 }
 
 void loop() {
   if(lose_game){
     int colors[100] = {};
     lose_game = false;
+    sequence_led = 0;
     level = 0;}
   
   addSequence();
   sequence();
-  
+  waitPlayer();
 }
--- commands.h
+++ commands.h
@@ -10,6 +10,39 @@ void sequence(){
 
 void addSequence(){
   Serial.println(level);
-  int num = random(2,4);
+  int num = random(2,6);
   colors[level++] = num;
+  }
+
+void waitPlayer(){
+  for(int i = 0;i<level;++i){
+    waitPlay();
+    checkSequence();
+    if(lose_game){break;}
+    sequence_led++;
+    }
+    sequence_led = 0;
+  }
+
+void waitPlay(){
+  boolean played = false;
+  while(!played){
+    for(int i = 0;i<4;++i){
+      if(digitalRead(buttons[i]) == HIGH){
+        button_on = i ;
+        digitalWrite(leds[i], HIGH);
+        delay(1000);
+        digitalWrite(leds[i], LOW);
+        played = true;
+        }
+      }
+      delay(10);
+    }
+  }
+
+void checkSequence() {
+    if (colors[sequence_led] != button_on) {
+      // GAME OVER.
+      lose_game = true;
+    }
   }
